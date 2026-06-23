31076a60a23008edf1ca38f6390af1d4f87459b3
Junior SEVERE <severejunior2017@gmail.com>
2026-06-22 22:27:25 -0400

    begin chap3

---
+++ june2026/revise_python/chap3/file1.py
@@ -0,0 +1,9 @@
+temperature = int(input("Ecrire la temperature: "))
+
+if temperature < 20:
+    print("Froid")
+elif temperature >= 20 and temperature < 30:
+    print("Chaud")
+elif temperature >= 30:
+    print("Tres chaudjjjjjjj")
+            
